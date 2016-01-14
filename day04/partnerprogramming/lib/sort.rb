3973bb1eeec5919b1793f206f987f5194955581c
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-01-14 18:06:55 -0500

    Cleaned up lines

--- day04/partnerprogramming/lib/sort.rb
+++ day04/partnerprogramming/lib/sort.rb
@@ -12,32 +12,22 @@ class Sort
 		while (@newword != @word2)
 			num = 0	
 			while num < @word1.length
-			if @word1[num] == @word2[num]
-				@newword[num] = @word2[num]
-								
-			else
-				@letterold=@word1[num]
-				@newword[num] = @word2[num]
-				
-					if @dictionary.exists?(@newword) == true
-					#@newword = @word2
-					puts @newword
-					else
-					@newword[num]=@letterold
-					end
-			end
+				if @word1[num] == @word2[num]
+					@newword[num] = @word2[num]
+									
+				else
+					@letterold=@word1[num]
+					@newword[num] = @word2[num]
+					
+						if @dictionary.exists?(@newword) == true
+						puts @newword
+						else
+						@newword[num]=@letterold
+						end
+				end
 			num += 1
 			end		
 		sort
 		end
-
-        
-
 	end
 end
-
-
-	# def doesitexists(newword)
-
-	# # dictionary.exists?(newword)	
-	# end

63f74ea18dc4badf1228a2012248a5746ea30aff
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-01-15 11:24:06 -0500

    Added bishop.rb and rook.rb

--- day05/app.rb
+++ day05/app.rb
@@ -1,7 +1,18 @@
 require_relative("lib/king.rb")
+require_relative("lib/rook.rb")
+require_relative("lib/bishop.rb")
 
-black_king= King.new(5, 8, "black")
 white_king = King.new(5, 1, "white")
+black_king= King.new(5, 8, "black")
 
-# Test
-p white_king.move?(5,2)
+white_rook_left = Rook.new(1, 1, "white`")
+white_rook_right = Rook.new(8, 1, "white")
+black_rook_left = Rook.new(1 , 8, "black")
+black_rook_right = Rook.new(8, 8, "black")
+
+p black_rook_left.move?(1,10)
+p black_rook_right.move?(2,3)
+
+white_bishop_left = Bishop.new(3, 1, "white")
+white_bishop_right = Bishop.new(6, 1, "white" )
+

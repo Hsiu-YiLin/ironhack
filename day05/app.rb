95cb880569460733e28e148b5ecb0e8b920fbbb5
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-01-15 11:44:36 -0500

    Added Knight class

--- day05/app.rb
+++ day05/app.rb
@@ -1,6 +1,7 @@
 require_relative("lib/king.rb")
 require_relative("lib/rook.rb")
 require_relative("lib/bishop.rb")
+require_relative("lib/knight.rb")
 
 white_king = King.new(5, 1, "white")
 black_king= King.new(5, 8, "black")
@@ -10,9 +11,16 @@ white_rook_right = Rook.new(8, 1, "white")
 black_rook_left = Rook.new(1 , 8, "black")
 black_rook_right = Rook.new(8, 8, "black")
 
-p black_rook_left.move?(1,10)
-p black_rook_right.move?(2,3)
-
 white_bishop_left = Bishop.new(3, 1, "white")
-white_bishop_right = Bishop.new(6, 1, "white" )
+white_bishop_right = Bishop.new(6, 1, "white")
+black_bishop_left = Bishop.new(3, 8, "black")
+black_bishop_right = Bishop.new(6, 8, "black")
 
+white_knight_left =  Knight.new(2, 1, "white")
+white_knight_right =  Knight.new(7, 1, "white")
+black_knight_left = Knight.new(2, 8, "black") 
+black_knight_right = Knight.new(7, 8, "black")
+
+p white_knight_left.move?(1, 3)
+p white_knight_left.move?(3, 3)
+p white_knight_left.move?(4, 2)
--- day05/lib/knight.rb
+++ day05/lib/knight.rb
@@ -1,4 +1,4 @@
-class Bishop
+class Knight
 	def initialize(pos_x, pos_y, color)
 		@pos_x = pos_x
 		@pos_y = pos_y
@@ -10,7 +10,7 @@ class Bishop
 		dy = (dest_y - @pos_y).abs
 		puts dx
 		puts dy
-		if dx==dy
+		if (dx== 1 && dy==2) || (dx==2 && dy ==1)
 			true
 				else
 					false

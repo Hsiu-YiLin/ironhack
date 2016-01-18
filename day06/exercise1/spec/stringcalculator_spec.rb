2a31be464d071c7ef0c5bb9b667f38c91143afc4
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-01-18 12:36:58 -0500

    Add before :each do to RSpect

--- day06/exercise1/spec/stringcalculator_spec.rb
+++ day06/exercise1/spec/stringcalculator_spec.rb
@@ -1,21 +1,22 @@
 require_relative("../lib/stringcalculator.rb")
 
-RSpec.describe StringCalculator do 
+RSpec.describe StringCalculator do
+	before :each do
+		@string_calculator = StringCalculator.new
+	end
+
 	it "returns 0 for empty string" do
 		# p string_calculator.add("") == 0
-		string_calculator = StringCalculator.new
-		expect(string_calculator.add("")).to eq(0)
+		expect(@string_calculator.add("")).to eq(0)
 	end
 
 	it "returns one number" do
-		string_calculator = StringCalculator.new
-		expect(string_calculator.add("9")).to eq(9)
-		expect(string_calculator.add("5")).to eq(5)
+		expect(@string_calculator.add("9")).to eq(9)
+		expect(@string_calculator.add("5")).to eq(5)
 	end
 
 	it "returns a long number" do
-		string_calculator = StringCalculator.new
-		expect(string_calculator.add("123129127391, 234234234234")).to eq(357363361625)
+		expect(@string_calculator.add("123129127391, 234234234234")).to eq(357363361625)
 	end
 end
 

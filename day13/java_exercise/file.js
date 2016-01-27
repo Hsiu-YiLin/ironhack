b212540787d62e8768120bd895b3fe05f215d578
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-01-27 11:49:20 -0500

    Changed .length

--- day13/java_exercise/file.js
+++ day13/java_exercise/file.js
@@ -5,14 +5,15 @@ var results = process(exampleArray);
 function process(input) {
 	var two=0;
 	var string = [];
-	for (var i=0;i<7;i+=1){
-		while(two<7) {
+	// input.forEach(function(entry)
+	for (var i=0;i<input.length;i+=1){
+		while(two<input.length) {
 
 			// if (input[two] + input[i] == 0)
-			if (input[i] + input[two] == 0){
+			if (input[i] + input[two] === 0){
 				// if (string[i] == (two+","+i)) {
 					string.push (i+","+two);
-				}
+				};
 
 			// }
 		two+=1;

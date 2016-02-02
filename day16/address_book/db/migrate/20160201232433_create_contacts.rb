848a8a3749bad673b93362da199d1cf2e5f9329d
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-02 15:53:12 -0500

    Updated Address Book basic function

--- day16/address_book/db/migrate/20160201232433_create_contacts.rb
+++ day16/address_book/db/migrate/20160201232433_create_contacts.rb
@@ -1,8 +1,11 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-
-      t.timestamps null: false
+    	t.string :name
+    	t.string :address
+    	t.string :phone_number
+    	t.string :email
+      	t.timestamps null: false
     end
   end
 end
--- day16/address_book/db/schema.rb
+++ day16/address_book/db/schema.rb
@@ -14,8 +14,12 @@
 ActiveRecord::Schema.define(version: 20160202142038) do
 
   create_table "contacts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone_number"
+    t.string   "email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end

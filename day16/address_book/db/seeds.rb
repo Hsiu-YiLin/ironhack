a505266c94f9b6d069b14098bba60bfc7eeee423
Hsiu-YiLin <hlin004@fiu.edu>
2016-02-01 23:42:01 -0500

    REVERT commit

---
+++ day16/address_book/db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Contact.create name: "TestName", address: "TestAddress", phone_number: "1234567890", email: "test@test.com"

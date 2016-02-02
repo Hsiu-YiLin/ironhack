f7c57036ea39197b5dce543a5109c88c0ae7f1a1
Hsiu-YiLin <hlin004@fiu.edu>
2016-02-01 23:47:32 -0500

    Resolve merge conflicts

--- day16/address_book/app/controllers/contacts_controller.rb
+++ day16/address_book/app/controllers/contacts_controller.rb
@@ -1,9 +1,6 @@
 class ContactsController < ApplicationController
 	def index
-<<<<<<< HEAD
 	@contacts = Contact.all
-=======
->>>>>>> a7be304503e3fb57d89bf98492dbac485d899dd7
 	render "index"
 	end
 end

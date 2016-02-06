e36768a2208fbb51a93b18f07b060f46a3421385
Hsiu-YiLin <hlin004@fiu.edu>
2016-02-06 13:16:43 -0500

    Added show for each concert

--- week04/music_concerts_network/app/controllers/concerts_controller.rb
+++ week04/music_concerts_network/app/controllers/concerts_controller.rb
@@ -1,12 +1,24 @@
 class ConcertsController < ApplicationController
 
 	def index
-		@concerts = Concert.all
+		@concerts_today = Concert.where("date < ?", Date.tomorrow)
+		@concerts_latermonth = Concert.where("date > ?", Date.tomorrow).where("date < ?", DateTime.end_of_month)
+	# concerts_all.each do |concert|
+	# 	if concert.date < DateTime.current.end_of_day
+	# 		@concerts_today.push(concert)
+	# 	elsif (concert.date > DateTime.end_of_month) && (concert.date < DateTime.current.end_of_day)
+	# 		@concerts_latermonth << concert
+	# 	end
+	# end
+
 	end
 
 	def edit
 		@concert = Concert.find_by(id: params[:id])
+	end
 
+	def show
+		@concert = Concert.find_by(id: params[:id])
 	end
 	
 end
--- week04/music_concerts_network/db/seeds.rb
+++ week04/music_concerts_network/db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Concert.create(artist: "HAIM", city: "Chicago", date: "2016-02-05 17:07", price: 40.00, description: "HAIM DESCRIPTION")
-
-Concert.create(artist: "Santigold", city: "Austin", date: "2016-05-05 20:00", price: 65.50, description: "DESCRIPTION")
+Concert.create(artist: "HAIM", city: "Chicago", date: "2016-02-21 17:07", price: 40.00, description: "HAIM DESCRIPTION")
+Concert.create(artist: "Rick", city: "Miami", date: "2016-02-05", price: 0,description: "Rick is playing") 
+Concert.create(artist: "Santigold", city: "Austin", date: "2016-05-15 20:00", price: 65.50, description: "DESCRIPTION")
 
 # Concert.create(artist:
 

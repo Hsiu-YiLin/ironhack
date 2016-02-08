38c0ceb67f0104f48dc1990082e59ed46b407fe9
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-08 17:51:47 -0500

    Remove price lock

--- week05/day21/pizza-builder-master/src/pizza.js
+++ week05/day21/pizza-builder-master/src/pizza.js
@@ -2,22 +2,31 @@
 $(document).ready(function(){
 	$(".btn-pepperonni.active").on("click", function(){
 		$(".pep").toggle();
+		$(".btn-pepperonni").toggleClass("active");
 	});
 
 	$(".btn-mushrooms").on("click", function(){
 		$(".mushroom").toggle();
+		$(".btn-mushrooms").toggleClass("active");
 	});
 
 	$(".btn-green-peppers").on("click", function(){
 		$(".green-pepper").toggle();
+		$(".btn-green-peppers").toggleClass("active");
 	});
 
 	$(".btn-crust").on("click", function(){
 		$(".crust").toggleClass("crust-gluten-free");
+		$(".btn-crust").toggleClass("active");
 	});
 
 	$(".btn-sauce").on("click", function(){
 		$(".sauce").toggleClass("sauce-white");
+		$(".btn-sauce").toggleClass(" active ");
 	});
+
+	
+
+
 	
 });

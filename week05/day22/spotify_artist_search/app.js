a8ff203c7485b565e3cb91882150b622121c3dd0
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-09 16:49:36 -0500

    Save point

--- week05/day22/spotify_artist_search/app.js
+++ week05/day22/spotify_artist_search/app.js
@@ -1,21 +1,38 @@
 $(document).on("ready", function() {
+
 $('.js-character-form-submit').on('click', onSubmit);
+
 });
 
 function onSubmit (event) {
-
+	console.log(event);
 	event.preventDefault();
 	console.debug('SUBMITTED');
 
 	var artistsearch = $("#artist").val();
-	
+
 	$.ajax({
 		url: "https://api.spotify.com/v1/search?type=artist&query="+artistsearch,
 		success: function (response) {
-		  console.log(response);
+		  console.log(response.artists.items);
+		  displayArtistImage(response.artists.items);
 		 },
 		error: function () {
 		  alert("You broke Spotify.");
 		}
+	});
+}	
+
+function displayArtistImage (artistresult){
+	artistresult.forEach(function(artist){
+		console.log(artist.images.);
+		var html = `
+		<li>
+		${artist.name}
+		
+
+		</li>
+		`;
+		$(".artist-display").append(html);
 	});
 }

43a88071fc0881816a80931a5f5e7fa0c784a85e
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-12 14:52:05 -0500

    Added Album Art

--- week05/day25/spotifyPlayerSkeleton-master/app.js
+++ week05/day25/spotifyPlayerSkeleton-master/app.js
@@ -7,17 +7,40 @@ function tracksubmit(event){
 	// Prevent form from redirecting.
 	event.preventDefault();
 	
-	console.log(event);
+	// console.log(event);
 	var track = $("#track").val();
-	console.log(track);
+	// console.log(track);
+
+
 	$.ajax({
 		url: "https://api.spotify.com/v1/search?type=track&q="+track,
 		success: function(response){
-			console.log(response);
-
+			displayTrack(response.tracks.items[0]);
 		},
 		error: function(){
-			console.log("Error track");
+			console.log("Error search");
 		}
 	});
+};
+
+function displayTrack(trackinfo){
+	// console.log(trackinfo);
+	$(".js-title").text(trackinfo.name);
+	
+	makeArtist(trackinfo.artists);	
+	displayAlbum(trackinfo);
+	console.log(trackinfo);
+};
+
+function makeArtist(artistarray){
+	var artistnew = "";
+	(artistarray).forEach(function(artist){
+		artistnew=artistnew + artist.name + "/";
+	});
+	$(".js-artist").text(artistnew);
+};
+
+function displayAlbum(trackinfo){
+	var htmlart=`<img src="${trackinfo.album.images[0].url}">`;
+	$(".cover").html(htmlart);
 };

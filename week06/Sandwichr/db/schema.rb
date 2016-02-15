9c174b368d30d74ce2acba011ee9f016b7a83fd1
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-15 12:20:14 -0500

    Added Ingredient Resources and Routes

--- week06/Sandwichr/db/schema.rb
+++ week06/Sandwichr/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215155306) do
+ActiveRecord::Schema.define(version: 20160215170933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "calories"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sandwiches", force: :cascade do |t|
     t.string   "name"

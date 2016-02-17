9f0b5de1f52b5eea3e3eef8f3d02a419ffadd9c7
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-17 11:01:48 -0500

    Added admin function

--- week06/day27/auth-exercise-master/app/controllers/application_controller.rb
+++ week06/day27/auth-exercise-master/app/controllers/application_controller.rb
@@ -15,8 +15,14 @@ class ApplicationController < ActionController::Base
     unless current_user
 
       flash[:message] = 'Please log in or register to access this page'
-      redirect_to root_path
+      redirect_to '/'
     end
   end
 
+  def admin_only
+    unless current_user && current_user.role == "admin"
+      flash[:access_denied] = "Access denied. You must be admin to see this page."
+      redirect_to '/'
+    end
+  end
 end

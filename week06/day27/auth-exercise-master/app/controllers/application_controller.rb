34535009a105006c6a76e79e3165f3be8e5498db
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-02-17 10:08:16 -0500

    Added authorize user

--- week06/day27/auth-exercise-master/app/controllers/application_controller.rb
+++ week06/day27/auth-exercise-master/app/controllers/application_controller.rb
@@ -9,5 +9,14 @@ class ApplicationController < ActionController::Base
   		@current_user = User.find_by(id: session[:user_id])
   	end
   	@current_user
-  end  
+  end 
+
+  def authorize_user
+    unless current_user
+
+      flash[:message] = 'Please log in or register to access this page'
+      redirect_to root_path
+    end
+  end
+
 end
